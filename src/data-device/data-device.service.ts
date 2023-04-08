821d83d063c9248cca050a6d6ad94e7d21d497ff
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2023-04-08 14:54:40 +0700

    fix: decode handling realtime data

--- src/data-device/data-device.service.ts
+++ src/data-device/data-device.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InfluxProvider } from './providers/influx.provider';
 
 @Injectable()
@@ -28,7 +28,7 @@ export class DataDeviceService {
       dataId: (enableDecode) ? dataId : '-'
     }
 
-    let fluxQuery = `
+    const fluxQuery = `
     from(bucket: "teltonika")
       |> range(start: ${startTime}, stop: ${endTime})
       |> filter(fn: (r) => r["_measurement"] == "${imei}")
@@ -50,14 +50,13 @@ export class DataDeviceService {
     const returnInflux = await this.influx.readPoints(fluxQuery)
 
     const dataAfterDecode = this.decode(returnInflux, parseInt(maskingBit), enableDecode, dataId)
-
+    if (dataAfterDecode.length === 0) throw new NotFoundException('History data not found')
     response['dataCount'] = dataAfterDecode.length
     response['data'] = dataAfterDecode
     return response
   }
 
   async getLast(params: any) {
-    return 'Maaf API belum support'
     const imei = params.imei
     const avlId = params.avlId
     const enableDecode = params.enableDecode === "true" ? true : false
@@ -74,33 +73,38 @@ export class DataDeviceService {
       dataId: (enableDecode) ? dataId : '-'
     }
 
-    let fluxQuery = `
-    from(bucket: "teltonika")
-      |> range(start: 0)
-      |> filter(fn: (r) => r["_measurement"] == "${imei}")
-      |> filter(fn: (r) => r["ioID"] == "${avlId}")
-      |> last()
-      |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
-      |> map(fn: (r) => ({
-        AVLValue : r.ioValue,
-        AVLTimestamp : r._time,
-        altitude: r.altitude,
-        longitude : r.longitude,
-        latitude : r.latitude,
-        angle : r.angle,
-        satellites : r.satellites,
-        speed : r.speed,
-        priority: r.priority,
-        eventTriggered: r.event,
-        storedTime : r.storedTime }))
+    let dataAfterDecode: Array<any> = []
+
+    for (let index = 1; index < 18; index++) {
+      const time = Math.round(1.1 ** (index * 10))
+
+      const fluxQuery = `
+      from(bucket: "teltonika")
+        |> range(start: -${time}s )
+        |> filter(fn: (r) => r["_measurement"] == "${imei}")
+        |> filter(fn: (r) => r["ioID"] == "${avlId}")
+        |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
+        |> map(fn: (r) => ({
+          AVLValue : r.ioValue,
+          AVLTimestamp : r._time,
+          altitude: r.altitude,
+          longitude : r.longitude,
+          latitude : r.latitude,
+          angle : r.angle,
+          satellites : r.satellites,
+          speed : r.speed,
+          priority: r.priority,
+          eventTriggered: r.event,
+          storedTime : r.storedTime }))
       `
+      const returnInflux = await this.influx.readPoints(fluxQuery)
+      dataAfterDecode = this.decode(returnInflux, parseInt(maskingBit), enableDecode, dataId)
+      if (dataAfterDecode.length > 0) break
+    }
+    if (dataAfterDecode.length === 0) throw new NotFoundException('Realtime data not found')
 
-    const returnInflux = await this.influx.readPoints(fluxQuery)
-
-    const dataAfterDecode = this.decode(returnInflux, parseInt(maskingBit), enableDecode, dataId)
-
-    response['dataCount'] = dataAfterDecode.length
-    response['data'] = dataAfterDecode
+    response['dataCount'] = 1
+    response['data'] = dataAfterDecode[dataAfterDecode.length - 1]
     return response
   }
 
